534a44a7e2f8d8c7fe0d288c8fa210b1bc4e5306
banboll <banboll@outlook.com>
2019-01-19 22:44:16 +0800

    init project

---
+++ src/main/java/com/hpin/assistant/AssistantApplication.java
@@ -0,0 +1,14 @@
+package com.hpin.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssistantApplication.class, args);
+    }
+
+}
+
